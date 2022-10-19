e494955c531b3023a0398b78be435728b1ea13e3
Han Long Eugene, Goh <hanlong2102@hotmail.com>
2022-10-20 00:24:15 +0800

    fridge page
    added appbar

--- lib/screens/home/fridge_page.dart
+++ lib/screens/home/fridge_page.dart
@@ -1,6 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+/* 
+Firestore.instance.collection("you_Collection_Path").add({
+  "key":value //your data which will be added to the collection and collection will be created after this
+}).then((_){
+  print("collection created");
+}).catchError((_){
+  print("an error occured");
+});
+*/
+
 class FridgePage extends StatefulWidget {
   const FridgePage({super.key});
 
@@ -15,38 +25,60 @@ class _FridgePageState extends State<FridgePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+            title: Text('MunchPouch'),
+            backgroundColor: Colors.indigo[400],
+            elevation: 0.0,
+            actions: <Widget>[
+              TextButton.icon(
+                  icon: Icon(Icons.person),
+                  label: Text('logout'),
+                  onPressed: () async {
+                    print("Logout pressed");
+                  }),
+              TextButton.icon(
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
+                onPressed: () async {
+                  print("Settings pressed");
+                },
+              )
+            ]),
         body: StreamBuilder(
-      stream: _fridges.snapshots(),
-      builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-        if (streamSnapshot.hasData) {
-          return ListView.builder(
-              itemCount: streamSnapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                final DocumentSnapshot documentSnapshot =
-                    streamSnapshot.data!.docs[index];
-                return Card(
-                    margin: const EdgeInsets.all(10),
-                    child: ListTile(
-                        title: Row(
-                          children: <Widget>[
-                            Expanded(child: Text(documentSnapshot['product'])),
-                            Expanded(child: Text(documentSnapshot['belongsTo']))
-                          ],
-                        ),
-                        subtitle: Row(
-                          children: <Widget>[
-                            Expanded(
-                                child: Text(documentSnapshot['dateOfPlacement']
-                                    .toString()))
-                          ],
-                        )));
-              });
-        } else {
-          return const Center(
-            child: Text("Test"),
-          );
-        }
-      },
-    ));
+          stream: _fridges.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                  itemCount: streamSnapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final DocumentSnapshot documentSnapshot =
+                        streamSnapshot.data!.docs[index];
+                    return Card(
+                        margin: const EdgeInsets.all(10),
+                        child: ListTile(
+                            title: Row(
+                              children: <Widget>[
+                                Expanded(
+                                    child: Text(documentSnapshot['product'])),
+                                Expanded(
+                                    child: Text(documentSnapshot['belongsTo']))
+                              ],
+                            ),
+                            subtitle: Row(
+                              children: <Widget>[
+                                Expanded(
+                                    child: Text(
+                                        documentSnapshot['dateOfPlacement']
+                                            .toString()))
+                              ],
+                            )));
+                  });
+            } else {
+              return const Center(
+                child: Text("Test"),
+              );
+            }
+          },
+        ));
   }
 }
